347f4cf40f8e7230ca8b62a4afb99bb44b35a7b9
Njerimacharia98 <njerimacharia05@gmail.com>
2023-04-26 16:50:55 +0300

    add DOM element

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const apiKey = 'da7944363589a5d4fbdb1e574abce6df';
-
-
+const searchInput = document.getElementById('search-input');
+const searchBtn = document.getElementById('search-btn');
+const movieList = document.getElementById('movie-list');

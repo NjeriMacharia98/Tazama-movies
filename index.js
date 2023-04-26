4081fc2e9976d9041cba6815e51e14aded20d19f
Njerimacharia98 <njerimacharia05@gmail.com>
2023-04-26 17:04:54 +0300

    fetch movies when search button is clicked

--- index.js
+++ index.js
@@ -23,3 +23,36 @@ fetch(endpoint)
     });
   })
   .catch(error => console.error(error));
+// Fetch movies when search button is clicked
+searchBtn.addEventListener('click', () => {
+  const query = searchInput.value.trim();
+  if (query) {
+    const endpoint = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`;
+    fetch(endpoint)
+      .then(response => response.json())
+      .then(data => {
+movieList.innerHTML = '';
+        data.results.forEach(movie => {
+          const li = document.createElement('li');
+          const title = document.createElement('h3');
+          title.textContent = movie.title;
+          li.appendChild(title);
+          if (movie.poster_path) {
+            const img = document.createElement('img');
+            img.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+            img.alt = `${movie.title} poster`;
+            li.appendChild(img);
+            movieList.appendChild(li); 
+        }
+        });
+      })
+      .catch(error => console.error(error));
+  }
+});
+
+
+
+
+
+
+
